91c2638611e71e337e3f2ec3c9ffc98a3adcf5ac
Igor Becz <igor.becz@gmail.com>
2019-04-26 23:01:37 +0200

    Some minor improvements to reading hex content of the file

--- main.c
+++ main.c
@@ -1,18 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void read_file_to_memory(unsigned char *ptr_to_memory){
+int read_file_to_memory(unsigned char *ptr_mem, char* path_to_file){
 	FILE *fp;
-	unsigned char buffer[10];
+	unsigned char buffer[16];
 
-   	fp = fopen("fishie.ch8", "rb");
+   	fp = fopen(path_to_file, "rb");
 
-   	fread(buffer,sizeof(buffer),1,fp); // read 10 bytes to our buffer
-   	
-   	for(int i = 0; i<10; i++){
-    	printf("%x ", buffer[i]); // prints a series of bytes
-    	ptr_to_memory[i+500] = buffer[i];
+   	if(fp == NULL){
+   		printf("Couldn't open: %s\n", path_to_file);
+   		return -1;
    	}
 
+   	int how_many_bytes_was_read;
+   	int ptr_mem_top = 500;
+
+   	while(!feof(fp)){
+   		how_many_bytes_was_read = fread(buffer,sizeof(unsigned char),sizeof(buffer),fp);
+   		
+   		if(how_many_bytes_was_read>0){
+   			//	printf("Read: %d bytes\n", how_many_bytes_was_read);
+
+   			for(int i = 0; i<how_many_bytes_was_read; i++){
+   				if(buffer[i]<16)
+   					printf("0");
+   				
+   				ptr_mem[ptr_mem_top] = buffer[i];
+   				printf("%x ", ptr_mem[ptr_mem_top]);
+   				ptr_mem_top++;
+   				}
+
+   				printf("\n");
+
+   			} 
+   	}
+
+   	return ptr_mem_top;
 	fclose(fp);
 }
 
@@ -35,13 +59,48 @@ int main(int argc, char const *argv[])
 	unsigned short opcode;			// register to store current opcode
 	unsigned short stack[16];
 
-	read_file_to_memory(memory_ptr);
+	// ???? its convenient but not sure if correct
+	int p_mem_top;	// points to the last index where something is written to memory
 
-	printf("\nhello\n");
-	for(int i = 500; i<510; i++){
 
-		printf("%x ", memory[i]);	
+	// erease all memory
+	for (int i = 0; i < 4096; ++i)
+	{
+		memory[i] = 0;
 	}
+
+	if(argc != 2){
+		printf("%s\n", "Wrong parameter. You need to specify file you want to load to.");
+		return -1;
+	}
+
+	char* path_to_file = malloc(strlen(argv[1]) + 1);
+	strcpy(path_to_file, argv[1]);
+	p_mem_top = read_file_to_memory(memory_ptr, path_to_file);
+
+	//handle file load error
+	if(p_mem_top == -1){
+		printf("File load error. \n");
+		return -1;
+	}
+
+	// print content of memory
+	printf("\n");
+	printf("%s\n", "W pamieci:");
+
+	int current_byte = 500;
+
+	do{
+		for (int i = 0; i < 16; ++i){
+			if(memory[current_byte+i] < 16)
+				printf("0");
+
+			printf("%x ", memory[current_byte+i]);
+			
+		}
+		current_byte+=16;
+		printf("\n");
+	}while(current_byte<p_mem_top);
 
 	printf("\n");
 
